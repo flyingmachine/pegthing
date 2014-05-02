7a272543f5ffc8dda9d607edb90b6db3b7479068
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2014-05-01 22:06:48 -0400

    more progress with creating graph

--- src/pegthing/core.clj
+++ src/pegthing/core.clj
@@ -1,9 +1,10 @@
 (ns pegthing.core
   (:gen-class))
 
+;; TODO explain why it's nice to start at 0
 (defn tri*
   "Generates lazy sequence of triangular numbers"
-  ([] (tri* 0 1))
+  ([] (tri* 0 0))
   ([sum n]
      (let [new-sum (+ sum n)]
        (cons new-sum (lazy-seq (tri* new-sum (inc n)))))))
@@ -15,27 +16,58 @@
   (= n (last (take-while #(>= n %) tri))))
 
 (def complementary-dirs
-  {3 0
-   4 1
-   5 2})
+  {2 5
+   3 6
+   4 1})
 
-(defn new-board
-  []
-  {})
+(defn join-right
+  [row-num board pos]
+  (assoc-in board [pos 2] (inc pos)))
 
-(defn adjacent-positions
-  [pos]
-  (if (triangular? pos)
-    [2 3]
-    [1 2 3]))
+(defn join-down-right
+  [row-num board pos]
+  (assoc-in board [pos 3] (+ pos row-num 1)))
+
+(defn join-down-left
+  [row-num board pos]
+  (assoc-in board [pos 4] (+ pos row-num)))
+
+(defn adjacent-joiners
+  [board row-num]
+  [(partial join-right row-num)
+   (partial join-down-left row-num)
+   (partial join-down-right row-num)])
 
 (defn add-peg
-  [board row-num]
-  )
+  [board row-num pos joiners]
+  (let [joiners (if (triangular? pos) (rest joiners) joiners)
+        board (assoc-in board [pos :pegged] false)]
+    (reduce (fn [board joiner] (joiner board pos))
+            board
+            joiners)))
+
+(defn peg-positions
+  [row-num]
+  (map inc (range (nth tri (dec row-num))
+                  (nth tri row-num))))
 
 (defn add-row
   [board row-num]
+  (reduce add-peg board (peg-positions row-num) (adjacent-joiners board row-num)))
+
+(defn add-rows
+  [board rows]
+  (reduce add-row board (range 1 rows)))
+
+(defn add-last-row
+  [board rows]
   )
+
+(defn new-board
+  [rows]
+  (let [board {}]
+    (merge (add-rows board rows)
+           (add-last-row board rows))))
 
 (defn bigraph-assoc
   [board p1 p2 dir])
