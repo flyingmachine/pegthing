b5d24d0151496a2be5598be43df988e113900cc2
J. Brian Jordan <najitaka@gmail.com>
2020-11-21 20:02:26 -0600

    update clojure and remove unused set

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]]
   :main ^:skip-aot pegthing.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
